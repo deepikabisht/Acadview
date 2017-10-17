0a5acb8dd4015aaec0027241634a1dc386e5cb23
deepikabisht <bishtdeepika65@gmal.com>
2017-10-17 08:02:37 +0530

    Self Info

--- InstaBot.py
+++ InstaBot.py
@@ -1,3 +1,10 @@
 import requests
-BASE_URL=
-ACCESS_TOKEN=
+BASE_URL= 'https://api.instagram.com/v1/'
+APP_ACCESS_TOKEN= '4870715640.a48e759.874aba351e5147eca8a9d36b9688f494'
+def self_info():
+    request_url= (BASE_URL + 'users/self/?access_token=%s') % (APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+
+self_info()
