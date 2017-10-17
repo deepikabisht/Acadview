32f1034f1fe04b7126ab6a3c829839007aa1d11c
deepikabisht <bishtdeepika65@gmal.com>
2017-10-17 20:39:48 +0530

    Download user posted image by username

--- InstaBot.py
+++ InstaBot.py
@@ -60,7 +60,7 @@ def get_user_info(insta_username):
     else:
         print 'Status code other than 200 received!'
 
-#Function to get our own recent post
+#Function to download our  own posted image
 
 def get_own_post():
     request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (APP_ACCESS_TOKEN)
@@ -78,7 +78,7 @@ def get_own_post():
     else:
         print 'Status code other than 200 received!'
 
-# Function to fetch recent post of user by username
+# Function to fetch recent image  posted by user by its username
 
 def get_user_post(insta_username):
     user_id=get_user_id(insta_username)
@@ -90,7 +90,11 @@ def get_user_post(insta_username):
     user_media = requests.get(request_url).json()
     if(user_media['meta']['code']==200):
         if len(user_media['data']):
-            print user_media['data'][0]['id']
+            image_name = user_media['data'][0]['id'] + '.jpeg'
+            image_url = user_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'Your image has been downloaded!'
+
         else:
             print  'Post does not exists'
     else:
