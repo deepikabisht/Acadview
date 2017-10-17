c521656b9f66a1ba6b4550a286d3146b703c1eb9
deepikabisht <bishtdeepika65@gmal.com>
2017-10-17 18:58:41 +0530

    Function to fetch own recent post

--- InstaBot.py
+++ InstaBot.py
@@ -48,7 +48,8 @@ def get_user_info(insta_username):
     user_info= requests.get(request_url).json()
     if(user_info['meta']['code']==200):
         if len(user_info['data']):
-            print 'Username= %s ' %(user_info['data']['username'])
+            print 'User id: %s '% (user_id)
+            print 'Username: %s ' %(user_info['data']['username'])
             print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
             print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
             print 'No. of posts: %s' % (user_info['data']['counts']['media'])
@@ -56,6 +57,22 @@ def get_user_info(insta_username):
             print 'User does not exist!'
     else:
         print 'Status code other than 200 received!'
+
+#Function to get our own recent post
+
+def get_own_post():
+    request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    own_media = requests.get(request_url).json()
+    if(own_media['meta']['code']==200):
+        if len(own_media['data']):
+            print own_media['data'][0]['id']
+        else:
+            print  'Post does not exists'
+    else:
+        print 'Status code other than 200 received!'
+
+
 
 # InstaBot menu
 
@@ -66,6 +83,7 @@ def start_bot():
         print 'Here are your menu options:'
         print "a.Get your own details\n"
         print "b.Get details of a user by username\n"
+        print "c.Get your own recent post\n"
         print "j.Exit"
 
         choice=raw_input("Enter you choice: ")
@@ -74,6 +92,8 @@ def start_bot():
         elif choice=="b":
             insta_username = raw_input("Enter the username of the user: ")
             get_user_info(insta_username)
+        elif choice=="c":
+            get_own_post()
         elif choice=="j":
             exit()
         else:
